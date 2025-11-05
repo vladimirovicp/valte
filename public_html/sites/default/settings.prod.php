619aa4e013096920d5796b13f0869a48d06f3e51
vladimirovicp <vladimirovicp70@gmail.com>
2025-11-05 15:58:24 +0400

    feat: init

---
+++ public_html/sites/default/settings.prod.php
@@ -0,0 +1,19 @@
+ <?php
+ 
+$settings['trusted_host_patterns'] = [
+    '^example\.com$',
+    '^.+\.example\.com$',
+    '^example\.org$',
+    '^.+\.example\.org$',
+];
+
+// $databases['default']['default'] = [
+//   'database' => 'database_name',
+//   'username' => 'sql_username',
+//   'password' => 'sql_password',
+//   'host' => 'localhost',
+//   'port' => '3306',
+//   'driver' => 'mysql',
+//   'prefix' => '',
+//   'collation' => 'utf8mb4_general_ci',
+// ];
